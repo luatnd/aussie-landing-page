a31238ea3cf296ba3112037e181ec8daade87594
Luat Nguyen <neo.mxn0@gmail.com>
2017-12-03 12:49:06 +0700

    Responsive header for XL screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,21 +8,23 @@ import logo from '../../assets/images/logo-white.png'
 export default class Header extends Component {
 	render() {
 		return (
-			<header class={style.header}>
-				
-				<div class={style.logoC}>
-					<img src={logo} alt="Aussie logo"/>
+			<header class={style.headerC}>
+				<div class={style.header}>
+					
+					<div class={style.logoC}>
+						<img src={logo} alt="Aussie logo"/>
+					</div>
+					
+					<nav>
+						<Link activeClassName={style.active} href="/"><Text text="Home"/></Link>
+						<Link activeClassName={style.active} href="/profile"><Text text="Contact us"/></Link>
+						<Link activeClassName={style.active} href="/profile/john1"><Text text="Special Promotion 1"/></Link>
+						<Link activeClassName={style.active} href="/profile/john2"><Text text="Special Promotion 2"/></Link>
+						<Link activeClassName={style.active} href="/profile/john3"><Text text="Special Promotion 3"/></Link>
+						<Link activeClassName={style.active} href="/profile/john4"><Text text="Special Promotion 4"/></Link>
+					</nav>
+					
 				</div>
-				
-				<nav>
-					<Link activeClassName={style.active} href="/"><Text text="Home"/></Link>
-					<Link activeClassName={style.active} href="/profile"><Text text="Contact us"/></Link>
-					<Link activeClassName={style.active} href="/profile/john1"><Text text="Special Promotion 1"/></Link>
-					<Link activeClassName={style.active} href="/profile/john2"><Text text="Special Promotion 2"/></Link>
-					<Link activeClassName={style.active} href="/profile/john3"><Text text="Special Promotion 3"/></Link>
-					<Link activeClassName={style.active} href="/profile/john4"><Text text="Special Promotion 4"/></Link>
-				</nav>
-				
 			</header>
 		);
 	}
