ffa5e72a1c472bc8642efe87294049babb26f7ec
Luat Nguyen <neo.mxn0@gmail.com>
2017-12-04 18:21:44 +0700

    Initial promotion page

--- src/routes/PromotionPage/Promotion1/index.js
+++ src/routes/PromotionPage/Promotion1/index.js
@@ -1,32 +1,14 @@
 import { h, Component } from 'preact';
-import ParallaxImageBg from '../../components/ParallaxImageBg';
 
 import style from './style.scss';
-import preloadImg from '../../assets/images/aussiefreebieguru-4-xs-80.jpg';
 
 export default class PromotionPage extends Component {
-  
-  // Note: `promotionId` comes from the URL, courtesy of our router
-  render({promotionId}, state) {
+  render() {
     return (
-      <div class={style.promotionC}>
-        
-        <div class={style.promotionInner}>
-          
-          <ParallaxImageBg
-            height={670}
-            bgPreload={preloadImg}
-            bgHd={'./assets/images/aussiefreebieguru-4.jpg'}
-          >
-            <div class={style.mask}/>
-            
-            
-            <div>
-              PromotionCnt
-            </div>
-          </ParallaxImageBg>
-          
-        </div>
+      <div class={style.promoC}>
+      
+      
+        <h1>hello world</h1>
         
       </div>
     );
--- src/routes/PromotionPage/index.js
+++ src/routes/PromotionPage/index.js
@@ -1,6 +1,8 @@
 import { h, Component } from 'preact';
 import ParallaxImageBg from '../../components/ParallaxImageBg';
 
+import Promotion1 from './Promotion1';
+
 import style from './style.scss';
 import preloadImg from '../../assets/images/aussiefreebieguru-4-xs-80.jpg';
 
@@ -8,29 +10,33 @@ export default class PromotionPage extends Component {
   
   // Note: `promotionId` comes from the URL, courtesy of our router
   render({promotionId}, state) {
-    console.log("promotionId: ", promotionId);
+    const component = getPromotionComp(promotionId);
+    
     
     return (
       <div class={style.promotionC}>
-        
         <div class={style.promotionInner}>
-          
           <ParallaxImageBg
             height={670}
             bgPreload={preloadImg}
-            bgHd={'./assets/images/aussiefreebieguru-4.jpg'}
+            bgHd={'/assets/images/aussiefreebieguru-4.jpg'}
           >
             <div class={style.mask}/>
             
-            {/*TOOD: Load promotion here*/}
-            <h1>load promotion here</h1>
+            {component}
             
           </ParallaxImageBg>
-          
         </div>
-        
       </div>
     );
   }
 }
 
+function getPromotionComp(promotionId) {
+  switch (promotionId) {
+    case 'super-market-sale':
+      return <Promotion1/>;
+    default:
+      return <Promotion1/>;
+  }
+}
